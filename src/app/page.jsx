64847e17c457439ed26b07c02e849e23e5216caa
abdessamad Hamdaouia <103334428+Abdessamadhamdaoui@users.noreply.github.com>
2023-07-03 17:51:06 +0200

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,9 @@
-"use client"
  
+ "use client"
 import 'typeface-poppins';
 import "./font.css";
 import Headefordesktop from './component/Headfordesktop';
-import Headforphone from './component/Headforphone';
-import { useEffect, useState } from 'react';
+ 
 import Secend from './component/Secend';
  import Third from './component/Third'
 import Skills from './component/Skills';
@@ -14,25 +13,11 @@ import Projects from './component/Projects';
 import s, { projects } from './data/projects'
 import Contact from './component/Contact';
 export default function Home() {
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <=920);
-    };
-
-    handleResize();
-
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
+  
 
   return (
     <main className="" style={{ position: 'relative' }}>
-      {isMobile ? <Headforphone /> : <Headefordesktop />}
+        <Headefordesktop /> 
       <Secend/>
       <Third/>
       <Skills/> 
