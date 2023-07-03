57dee9b65aab22bf6532fa376468f26c91a9f4d2
abdessamad Hamdaouia <103334428+Abdessamadhamdaoui@users.noreply.github.com>
2023-07-03 14:05:51 +0200

    Create aboutData.js

---
+++ src/app/data/aboutData.js
@@ -0,0 +1,6 @@
+export const aboutData = {
+    title: "Who I am",
+    description1: "My name's hamdaoui. I'm a web designer and developer based in morroco, africa.",
+    description2: " a passionate Full Stack Developer With a diploma in Cloud Computing and a wealth of experience in both frontend and backend development, I possess the skills to create innovative and user-friendly web applications. ",
+    image:'https://dev-portfolio-template.netlify.app/static/media/eduOrange.c61dcfc3.svg'
+}
